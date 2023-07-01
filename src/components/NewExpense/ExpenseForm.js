a959b01d615733ee34f9a70c90a2f43efc244ea9
Ajeet Jha <kumarjhaajeet653@gmail.com>
2023-07-01 17:11:11 +0530

    form submit created

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,76 @@
 import './ExpenseForm.css';
+import { useState } from 'react';
 function ExpenseForm() {
+  const [enteredTitle, setenteredTitle] = useState('');
+  const [enteredAmount, setenteredAmount] = useState('');
+  const [enteredDate, setenteredDate] = useState('');
+  // const [userInput, setUserInput] = usestate({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // });
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setenteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // })
+    // setUserInput((prevState) =>{
+    //   return {
+    //   ...prevState,
+    //     enteredTitle: event.target.value
+    //   }
+    // })
   }
+  const amountChangeHandler = (event) => {
+    setenteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value
+    // })
+    // setUserInput((prevState) =>{
+    //   return {
+    //  ...prevState,
+    //     enteredAmount: event.target.value
+    //   }
+    // })
+  }
+  const dateChangeHandler = (event) => {
+      setenteredDate(event.target.value);
+      // setUserInput({
+      //  ...userInput,
+      //   enteredDate: event.target.value
+      // })
+      // setUserInput((prevState) =>{
+      //   return {
+      // ...prevState,
+      //     enteredDate: event.target.value
+      //   }
+      // })
+    }
+    const submitHandler = (event) =>{
+      event.preventDefault();
+      const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+      }
+      console.log(expenseData);
+    }
   return (
-   <form>
+   <form onSubmit={submitHandler}>
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" onChange={titleChangeHandler}/>
+        <input type="text" onChange={titleChangeHandler} />
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" />
+        <input type="number" onChange={amountChangeHandler} />
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" />
+        <input type="date" onChange={dateChangeHandler} />
       </div>
     </div>
     <div className="new-expense__actions">
